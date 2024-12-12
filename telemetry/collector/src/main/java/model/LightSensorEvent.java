f3085bd35b84c1279c238d93673013dd88f84d81
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2024-12-12 16:39:18 +0300

    Добавить базовый класс LightSensorEvent для датчика освещённости

---
+++ telemetry/collector/src/main/java/model/LightSensorEvent.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class LightSensorEvent extends SensorEvent {
+    private int linkQuality;
+    private int luminosity;
+
+    @Override
+    public SensorEventType getType() {
+        return SensorEventType.LIGHT_SENSOR_EVENT;
+    }
+} 
