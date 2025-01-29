15e79c9eb8e8c2df7b7708deac37e025bfcf92dc
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-01-29 16:40:12 +0300

    Добавить класс модели для Product

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/model/Product.java
@@ -0,0 +1,57 @@
+package ru.yandex.practicum.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.yandex.practicum.dto.ProductCategory;
+import ru.yandex.practicum.dto.QuantityState;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "products")
+@EqualsAndHashCode(of = "id")
+@ToString
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(columnDefinition = "UUID DEFAULT gen_random_uuid()")
+    private UUID id;
+
+    @Column(name = "name", nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String name;
+
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    @Size(min = 3, max = 1000)
+    private String description;
+
+    @Column(name = "image_src")
+    @Size(min = 3, max = 500)
+    private String imageSrc;
+
+    @Column(name = "quantity_state", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private QuantityState quantityState;
+
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
+    private ProductCategory category;
+
+    @Column(name = "price", nullable = false)
+    @NotNull
+    @DecimalMin(value = "1.0")
+    private BigDecimal price;
+}
