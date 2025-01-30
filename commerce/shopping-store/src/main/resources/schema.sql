ed7d52cbf741ecdc8cc454683f103887070683c2
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-01-30 09:16:26 +0300

    Добавить схему БД сервиса Shopping Store

---
+++ commerce/shopping-store/src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS products
+(
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    image_src VARCHAR(500),
+    quantity_state VARCHAR(6) NOT NULL,
+    category VARCHAR(8),
+    price DECIMAL(10, 2) NOT NULL
+);
