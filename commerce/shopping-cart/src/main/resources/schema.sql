b89e37e20a25eda2d9e012a900920b02ca20793e
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-02-04 12:29:41 +0300

    Добавить schema.sql для сервиса Shopping Cart

---
+++ commerce/shopping-cart/src/main/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS shopping_cart
+(
+    id UUID PRIMARY KEY NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shopping_cart_product
+(
+    id UUID PRIMARY KEY,
+    product_id UUID NOT NULL,
+    quantity BIGINT NOT NULL,
+    shopping_cart_id UUID NOT NULL,
+    FOREIGN KEY (shopping_cart_id) REFERENCES shopping_cart(id) ON DELETE CASCADE
+);
