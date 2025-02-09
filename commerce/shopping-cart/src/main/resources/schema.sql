4946d743f680fcfa2d5b58d001f01cd74873ef6f
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-02-09 10:00:41 +0300

    Исправить ошибку: поле state определено в таблице shopping_cart_product вместо shopping_cart

--- commerce/shopping-cart/src/main/resources/schema.sql
+++ commerce/shopping-cart/src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS shopping_cart
 (
     id UUID PRIMARY KEY NOT NULL,
-    username VARCHAR(100) NOT NULL UNIQUE
+    username VARCHAR(100) NOT NULL UNIQUE,
+    state VARCHAR(8) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS shopping_cart_product
@@ -10,6 +11,5 @@ CREATE TABLE IF NOT EXISTS shopping_cart_product
     product_id UUID NOT NULL,
     quantity BIGINT NOT NULL,
     shopping_cart_id UUID NOT NULL,
-    state VARCHAR(8) NOT NULL,
     FOREIGN KEY (shopping_cart_id) REFERENCES shopping_cart(id) ON DELETE CASCADE
 );
