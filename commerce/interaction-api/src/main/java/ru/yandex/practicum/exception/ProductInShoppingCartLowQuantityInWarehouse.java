d56908dfc15d31400d4dab70c65248c73d925e2b
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-02-10 14:25:43 +0300

    Заменить прием одного UUID на Set в конструкторе ProductInShoppingCartLowQuantityInWarehouse

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/exception/ProductInShoppingCartLowQuantityInWarehouse.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/exception/ProductInShoppingCartLowQuantityInWarehouse.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.exception;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -12,9 +13,9 @@ public class ProductInShoppingCartLowQuantityInWarehouse extends RuntimeExceptio
     private final HttpStatus httpStatus;
     private final String userMessage;
 
-    public ProductInShoppingCartLowQuantityInWarehouse(UUID productId) {
-        super("Товар из корзины с ID " + productId + " не находится в требуемом количестве на складе.");
+    public ProductInShoppingCartLowQuantityInWarehouse(Set<UUID> productIds) {
+        super("Товары из корзины с ID " + productIds + " не находится в требуемом количестве на складе.");
         this.httpStatus = HttpStatus.BAD_REQUEST;
-        this.userMessage = "Товар из корзины не находится в требуемом количестве на складе.";
+        this.userMessage = "Товары из корзины не находится в требуемом количестве на складе.";
     }
 }
