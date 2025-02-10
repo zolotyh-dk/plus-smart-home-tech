f904be6e9166fdfc47b31dd2bb6a461a328a9fee
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-02-10 14:26:25 +0300

    Добавить мапперы для товара и адреса

---
+++ commerce/warehouse/src/main/java/ru/yandex/practicum/mapper/AddressMapper.java
@@ -0,0 +1,18 @@
+package ru.yandex.practicum.mapper;
+
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+import org.springframework.stereotype.Component;
+import ru.yandex.practicum.dto.warehouse.AddressDto;
+
+@Component
+public class AddressMapper {
+    public AddressDto toDto(String address) {
+        return AddressDto.builder()
+                .country(address)
+                .city(address)
+                .street(address)
+                .house(address)
+                .flat(address)
+                .build();
+    }
+}
