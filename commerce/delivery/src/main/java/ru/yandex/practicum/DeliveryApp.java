75bddc11f490634d0a259d7d1341f20d89b6cc9c
Dmitry Zolotyh <zolotyh.dk@yandex.ru>
2025-03-04 06:07:38 +0300

    Добавить main классы приложений

--- commerce/delivery/src/main/java/ru/yandex/practicum/DeliveryApp.java
+++ commerce/delivery/src/main/java/ru/yandex/practicum/DeliveryApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Delivery {
+public class DeliveryApp {
     public static void main(String[] args) {
-        SpringApplication.run(Delivery.class, args);
+        SpringApplication.run(DeliveryApp.class, args);
     }
 }
---
+++ commerce/order/src/main/java/ru/yande/practicum/OrderApp.java
@@ -0,0 +1,11 @@
+package ru.yande.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApp.class, args);
+    }
+}
